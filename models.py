a5847a201b3c88fe5bee14427a7e40e687778b33
Ronak Mehta <ronakdm@uw.edu>
2021-05-27 18:00:21 -0700

    MyModel is now classification network.

--- models.py
+++ models.py
@@ -4,10 +4,16 @@ import torch.nn.functional as F
 
 # TODO
 class MyModel(nn.Module):
-    def __init__(self, input_dim, output_dim):
+    def __init__(self, input_dim, hidden_dim, output_dim):
         super(MyModel, self).__init__()
-        self.fc = nn.Linear(input_dim, output_dim)
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+        self.softmax = nn.Softmax(output_dim)
         pass
 
     def forward(self, x):
-        return F.relu(self.fc(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(x)
+        x = F.relu(self.fc2(x))
+        out = self.softmax(x)
+        return out
